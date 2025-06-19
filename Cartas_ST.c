b8996f34dd6ad8c349654ea9d28ee51d9b985004
RafaelBraga7 <100291415+RafaelBraga7@users.noreply.github.com>
2025-06-19 18:00:24 -0300

    Add files via upload

--- Cartas_ST.c
+++ Cartas_ST.c
@@ -10,6 +10,17 @@ int main() {
     float pib;
     int numPontosTur;
 
+    char nomeCarta2[30] = "ALEMANHA";
+    char estado2[1];
+    char codigoCarta2[3];
+    char nomeCidade2[20];
+    int populacao2;
+    float area2;
+    float pib2;
+    int numPontosTur2;
+
+
+
     printf("Carta1: %s\n",nomeCarta);
 
     printf("Informe uma letra entre A e H, representando um estado: \n");
@@ -33,6 +44,29 @@ int main() {
     printf("Informe a quantidade de pontos turisticos da cidade: \n");
     scanf("%d", &numPontosTur);
 
+    printf("Carta1: %s\n",nomeCarta2);
+
+    printf("Informe uma letra entre A e H, representando um estado: \n");
+    scanf("%s", &estado2);
+
+    printf("Informe a letra do estado seguida de um número de 01 a 04: \n");
+    scanf("%s", &codigoCarta2);
+
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", &nomeCidade2);
+
+    printf("Informe o número de habitantes da cidade: \n");
+    scanf("%d", &populacao2);
+
+    printf("Informe a área da cidade em KM²: \n");
+    scanf("%f", &area2);
+
+    printf("Informe o PIB da cidade: \n");
+    scanf("%f", &pib2);
+
+    printf("Informe a quantidade de pontos turisticos da cidade: \n");
+    scanf("%d", &numPontosTur2);
+
     printf("Carta 1: %s\n", nomeCarta);
     printf("Estado: %s\n", estado);
     printf("Código: %s\n", codigoCarta);
@@ -40,7 +74,16 @@ int main() {
     printf("População: %d\n", populacao);
     printf("Área: %f km²\n", area);
     printf("PIB: %f bilhões de reais\n", pib);
-    printf("Número de Pontos Turísticos: %d\n", numPontosTur);
+    printf("Número de Pontos Turísticos: %d\n\n", numPontosTur);
+
+    printf("Carta 2: %s\n", nomeCarta2);
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigoCarta2);
+    printf("Nome da cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f km²\n", area2);
+    printf("PIB: %f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", numPontosTur2);
 
     return 0;
 
